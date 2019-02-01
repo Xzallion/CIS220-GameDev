c715743969789d9222185272ebcf4c5d00b34338
Kenneth <gamereaperx@gmail.com>
2019-02-01 08:21:46 -0600

    Added Class work "Hello World"
    Initial Commit before next assignment.

---
+++ Hello World/Hello World/Assets/Scenes/CubeSpawner.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSpawner : MonoBehaviour {
+    public GameObject CubePrefabVar;
+
+	// Use this for initialization
+	void Start () {
+        //Instantiate(CubePrefabVar);	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Instantiate(CubePrefabVar);
+    }
+}
